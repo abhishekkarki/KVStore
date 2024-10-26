638865c15d866206584dbed76426e7c7a466296f
abhishekkarki <47475044+abhishekkarki@users.noreply.github.com>
2024-10-27 00:59:04 +0200

    Update config.h

--- main/config.h
+++ main/config.h
@@ -2,16 +2,16 @@
 #define CONFIG_H
 
 // Wi-Fi credentials
-#define CONFIG_WIFI_SSID "TP-Link_BA88"
-#define CONFIG_WIFI_PASS "91037695"
+#define CONFIG_WIFI_SSID "XXXXXXX"
+#define CONFIG_WIFI_PASS "XXXXXXX"
 
 // DHT Sensor GPIO Pin
 #define CONFIG_DHT_GPIO_PIN GPIO_NUM_22 // Replace with your GPIO number
 
 // MQTT Data
-#define CONFIG_MQTT_BROKER_URI "mqtt://192.168.0.104:1883"
-#define CONFIG_MQTT_USERNAME "edge_device"
-#define CONFIG_MQTT_PASSWORD "pass101"
+#define CONFIG_MQTT_BROKER_URI "mqtt://<ip>:<port>"
+#define CONFIG_MQTT_USERNAME "XXXXXX"
+#define CONFIG_MQTT_PASSWORD "XXXXXX"
 
 
 #endif // CONFIG_H
